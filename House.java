a5222083743f3df3dd955816c8eceb4c6dbb11da
Tomoko Hida <thida@smith.edu>
2023-12-22 11:46:46 -0500

    house

--- House.java
+++ House.java
@@ -1,9 +1,49 @@
 /* This is a stub for the House class */
-public class House {
+import java.util.*;
 
-  public House() {
+public class House extends Building {
+  private ArrayList<String> residents;
+  private boolean hasDiningRoom;
+
+  public House(String name, String address, int nFloors, ArrayList<String> residents) {
+    super(name, address, nFloors);
+    this.residents = new ArrayList<String>();
+    this.hasDiningRoom = hasDiningRoom;
     System.out.println("You have built a house: 🏠");
+
   }
+
+  public boolean hasDiningRoom(){
+    return this.hasDiningRoom = true;
+  }
+
+  public int nResidents(){
+    return this.nResidents();
+  }
+
+  public void moveIn(String name){
+    if (!this.residents.contains(name)){
+      throw new RuntimeException(this.name + " already lives in this house.");
+    } 
+    else this.residents.add(name);
+  }
+
+  public String moveOut(String name){
+    if (!this.residents.contains(name)){
+      return (this.name + " does not already live in this house.");
+    } 
+    else this.residents.remove(name);
+      return this.name;
+  }
+    boolean isResident;
+    public boolean isResident(String person){
+      if(this.residents.contains(person)){
+        return isResident = true;
+      }
+      else return isResident = false;
+      
+    }
+  
 
   public static void main(String[] args) {
     new House();
