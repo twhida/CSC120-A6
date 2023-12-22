40f8b045997c1e1eb0a786ed0f7d00d9323c8bda
Tomoko Hida <thida@smith.edu>
2023-12-22 14:20:25 -0500

    cafe

--- Cafe.java
+++ Cafe.java
@@ -7,30 +7,44 @@ public class Cafe extends Building{
     private int nSugarPackets; // The number of sugar packets remaining in inventory
     private int nCreams; // The number of "splashes" of cream remaining in inventory
     private int nCups; // The number of cups remaining in inventory
-    private ArrayList<int[]> inventory;
-    
-    public Cafe(String name, String address, int nFloors, ArrayList<int[]> inventory) {
-        super(name, address, nFloors);
-        this.nCoffeeOunces = 24;
-        this.nSugarPackets = 24;
-        this.nCreams = 24;
-        this.nCups = 4;
-        this.inventory =(this.inventory(this.nSugarPackets, this.nCreams, this.nCups));
-    }
-
-    private ArrayList<int[]> inventory(int nSugarPackets2, int nCreams2, int nCups2) {
+    private ArrayList<int[]> inventory(int CoffeeOunces, int nSugarPackets, int nCreams, int nCups){
         return null;
     }
     
+    public Cafe(String name, String address, int nFloors) {
+        super(name, address, nFloors);
+        this.nCoffeeOunces = 24;
+        this.nSugarPackets = 8;
+        this.nCreams = 8;
+        this.nCups = 4;
+        this.inventory(this.nCoffeeOunces, this.nSugarPackets, this.nCreams, this.nCups);
+    }
+
+
     public void sellCoffee(int nCoffeeOunces, int nSugarPackets, int Creams){
+        if(this.nCups >= 4){
         this.nCoffeeOunces -= 6;
         this.nSugarPackets -= 2;
         this.nCreams -= 2;
+        this.nCups -= 1;
+        }
+        else restock(nCoffeeOunces, nSugarPackets, nCreams, nCups);
 
     }
-    
+
+    private void restock(int nCoffeeOunces, int nSugarPackets, int nCreams, int nCups){
+        while (this.nCoffeeOunces <= 0){
+            this.nCoffeeOunces = 24;
+            this.nSugarPackets = 8;
+            this.nCreams = 8;
+            this.nCups = 4;
+        }
+        
+    }
+
+
     public static void main(String[] args) {
-        new Cafe();
+        new Cafe("The Mug", "1 Chapin Way", 1);
     }
     
 }
