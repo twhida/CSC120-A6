ccfb3dc99f89ab5ceb2c21d885386a5ff2b0040a
Tomoko Hida <thida@smith.edu>
2023-12-22 13:39:01 -0500

    update

--- Cafe.java
+++ Cafe.java
@@ -1,7 +1,11 @@
 /* This is a stub for the Cafe class */
-public class Cafe {
-
-    public Cafe() {
+public class Cafe extends Building{
+private int nCoffeeOunces;
+private int nSugarPackets;
+private int nCreams;
+private int nCups;
+    public Cafe(String name, String address, int nFloors) {
+        super(name, address, nFloors);
         System.out.println("You have built a cafe: ☕");
     }
     
